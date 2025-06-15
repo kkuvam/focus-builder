cad185c6033e800f32fc77615e0db233a2c5100c
Kuvam <60785625+kkuvam@users.noreply.github.com>
2025-06-15 10:34:46 +0530

    Updating model no. in api link

--- ai_generator.py
+++ ai_generator.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 class AIGenerator:
     def __init__(self):
-        self.api_url = "https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3"
+        self.api_url = "https://api-inference.huggingface.co/models/teknium/OpenHermes-2.5-Mistral-7B"
         self.headers = {
             "Authorization": f"Bearer {st.secrets['HF_API_KEY']}",
             "Content-Type": "application/json"
